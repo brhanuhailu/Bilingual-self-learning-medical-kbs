cc0f031b9aede943e5a68ff92e63dc4d459f6ed3
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-03-14 04:16:49 -0700

    Add files via upload

---
+++ storenglish.pl
@@ -0,0 +1,35 @@
+:-dynamic(diseases/2).
+:- dynamic diseases/2.
+
+diseases(moyzor, marasmus).
+diseases(yelm, diarrhea).
+diseases(haylom, unknown).
+diseases(gomto, kwashiorkor).
+diseases(goitomm, diarrhea).
+diseases(goitomm, unknown).
+diseases(letish, diarrhea).
+diseases(hail, kwashiorkor).
+diseases(hailish, kwashiorkor).
+diseases(mahi, kwashiorkor).
+diseases(mahi, unknown).
+diseases(hailish, unknown).
+diseases(hailelu, kwashiorkor).
+diseases(hailelu, unknown).
+diseases(doml, diarrhea).
+diseases(homlo, kwashiorkor).
+diseases(deryh, kwashiorkor).
+diseases(weldu, marasmus).
+diseases(weldu, unknown).
+diseases(hafut, kwashiorkor).
+diseases(hlku, kwashiorkor).
+diseases(yomsl, kwashiorkor).
+diseases(yomsl, unknown).
+diseases(hlku, unknown).
+diseases(hafut, unknown).
+diseases(swlan, unknown).
+diseases(chatlu, marasmus).
+diseases(chatlu, unknown).
+diseases(chultu, kwashiorkor).
+diseases(chultu, unknown).
+diseases(amanuel, kwashiorkor).
+
